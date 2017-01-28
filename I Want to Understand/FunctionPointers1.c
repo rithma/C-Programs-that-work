a2c189649f1430ea9addc9028e07f970fb53aa6b
rithma <rithers@gmail.com>
2017-01-28 13:28:56 -0800

    Update FunctionPointers1.c

--- I Want to Understand/FunctionPointers1.c
+++ I Want to Understand/FunctionPointers1.c
@@ -25,3 +25,24 @@ int main()
     return 0;
 }
 
+/*
+#include <stdio.h>
+// A normal function with an int parameter
+// and void return type
+void fun(int a)
+{
+    printf("Value of a is %d\n", a);
+}
+ 
+int main()
+{ 
+    void (*fun_ptr)(int) = fun;  // & removed
+ 
+    fun_ptr(10);  // * removed
+ 
+    return 0;
+}
+
+
+*/
+
